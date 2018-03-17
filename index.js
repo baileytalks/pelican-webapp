21c314664ad8dfaf098963c5e7764d40d68f676b
Bailey Kursar <bkursar@gmail.com>
2018-03-17 13:36:45 +0000

    Added the first page, with sign up form with username/password

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.listen(3000, function () {
+  console.log("Express server listening on port 3000");
+});
+
+app.get('/', function (req, res) {
+  res.render('pages/index');
+})
